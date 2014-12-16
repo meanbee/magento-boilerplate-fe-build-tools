4e1a75bafb88c6fda577944264fed123c6709858
Darren Belding <darren.belding@meanbee.com>
2014-12-16 17:30:04 +0000

    Add a sass task to the gulpfile for compilation of css

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,16 @@ var gulp = require('gulp'),
     plugins = require('gulp-load-plugins')();
 
 gulp.task('default', function() {
+
 });
 
 gulp.task('watch', function () {
+});
+
+gulp.task('sass', function () {
+    return gulp.src('src/scss/*.scss')
+        .pipe(plugins.sass({
+            outputStyle: 'expanded'
+        }))
+        .pipe(gulp.dest('css'));
 });
