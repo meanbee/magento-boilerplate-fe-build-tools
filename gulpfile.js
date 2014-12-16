7f1f470b8c9de4e277121c0e640de963d0235c74
Darren Belding <darren.belding@meanbee.com>
2014-12-16 17:33:03 +0000

    Update gulp sass task to include autoprefixr

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,17 @@ gulp.task('default', function() {
 });
 
 gulp.task('watch', function () {
+
 });
 
 gulp.task('sass', function () {
     return gulp.src('src/scss/*.scss')
         .pipe(plugins.sass({
             outputStyle: 'expanded'
+        }))
+        .pipe(plugins.autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
         }))
         .pipe(gulp.dest('css'));
 });
