9c58d4e177d06bd7b03353724acd6d835e8f9217
Darren Belding <darren.belding@meanbee.com>
2015-05-18 12:24:40 +0100

    BLT-6: Add font events library for loading font face

---
+++ src/components/fontfaceobserver/src/descriptors.js
@@ -0,0 +1,12 @@
+goog.provide('fontface.Descriptors');
+
+/**
+ * @typedef {{
+ *   style: (string|undefined),
+ *   weight: (string|undefined),
+ *   stretch: (string|undefined),
+ *   variant: (string|undefined),
+ *   featureSettings: (string|undefined)
+ * }}
+ */
+fontface.Descriptors;
